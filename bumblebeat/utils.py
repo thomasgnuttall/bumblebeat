4238bb30adc5f2deef9dab758efe462a7d5655b0
slimranking <thomasgnuttall@gmail.com>
2020-05-22 13:11:00 +0200

    funcs seperrated to modules

---
+++ bumblebeat/utils.py
@@ -0,0 +1,57 @@
+import os 
+
+import yaml 
+
+def load_yaml(fname):
+    """
+    Load yaml at <path> to dictionary, d
+
+    returns: dict
+    """
+    if not os.path.isfile(fname):
+        return None
+
+    with open(fname) as f:
+        conf = yaml.load(f, Loader=yaml.Loader)
+
+    filename = os.path.splitext(
+        os.path.basename(fname)
+    )[0]
+
+    return {filename: d}
+
+
+def split_range(r1, r2, n):
+    """
+    Split range <r1> - <r2> into <n> equal size buckets
+    """
+    step = (r2 - r1)/n
+    return [r1+step*i for i in range(n+1)]
+
+
+def get_bucket_number(value, srange):
+    """
+    Return index of bucket that <value> falls into
+
+    srange is a list of bucket divisions from split_range()
+    """
+    assert srange == (sorted(srange)),\
+        "srange must be sorted list"
+    assert len(set(srange)) == len(srange),\
+        "srange buckets must be unique"
+    assert value <= max(srange) and value >= min(srange),\
+        "value is not in any srange bucket"
+
+    for i in range(len(srange)-1):
+        if value <= srange[i+1]:
+            return i
+
+
+def create_dir_if_not_exists(path):
+    """
+    If the directory at <path> does not exist, create it empty
+    """
+    directory = os.path.dirname(path)
+    # Do not try and create directory if path is just a filename
+    if (not os.path.exists(directory)) and (directory != ''):
+        os.makedirs(directory)
---
+++ bumblebeat/vocabulary.py
@@ -0,0 +1,34 @@
+import itertools
+
+def create_vocab(n_instruments, n_velocity_buckets, first_index=5):
+    """
+    Create vocabulary of all possible instrument-velocity combinations.
+
+    <first_index> dictates which index to start on, default 5 to allow for
+    5 timestep tokens.
+
+    Each instrument is represented by <n_velocity_buckets> integers
+    Tokens increase across the dimension of instruments first:
+        token <first_index>     is instrument 0, velocity bucket 0
+        token <first_index> + 1 is instrument 0, velocity bucket 1
+        token <first_index> + 2 is instrument 0, velocity bucket 2
+        token <first_index> + N is instrument 0, velocity bucket 3
+            - where i = <n_instruments> - 1 (because of 0 indexing)
+            - where j = <n_velocity_buckets> - 1 (because of 0 indexing)
+            - where N = ixj - 1
+
+    returns: 2 x dict
+        {instrument_index: {velocity_index:token}}
+            ...for all instruments, velocities and tokens
+        {index: (instrument index, velocity index)},
+    """
+    
+    # itertools.product returns sorted how we desire    
+    all_comb = itertools.product(range(n_instruments), range(n_velocity_buckets))
+    d_reverse = {i+first_index:(x,y) for i,(x,y) in enumerate(all_comb)}
+    
+    d = {i:{} for i in range(n_instruments)}
+    for t, (i,v) in  d_reverse.items():
+        d[i][v] = t
+
+    return d, d_reverse
